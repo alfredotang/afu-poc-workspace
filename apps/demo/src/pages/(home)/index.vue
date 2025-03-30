6c76f1c4ae8e500d1d2900f8055533d24b2ac2c6
Alfredo Tang <asdzxcca0617@gmail.com>
2025-03-30 15:41:15 +0800

    chore: setup tailwind

--- apps/demo/src/pages/(home)/index.vue
+++ apps/demo/src/pages/(home)/index.vue
@@ -1,16 +1,23 @@
 <template>
   <div class="space-y-6">
+    <div class="text-highlight-primary text-2xl font-medium">
+      {{ route.meta.title }}
+    </div>
     <HomeDetail />
   </div>
 </template>
 
 <script setup lang="ts">
+import { useRoute } from 'vue-router'
+
 import HomeDetail from './_content/HomeDetail.vue'
 
 definePage({
   meta: {
-    title: 'Hello world',
+    title: 'Hello world, this is a demo page',
     isPublic: true,
   },
 })
+
+const route = useRoute()
 </script>
--- libs/styles/src/tailwind.base.ts
+++ libs/styles/src/tailwind.base.ts
@@ -1,13 +1,95 @@
 import type { Config } from 'tailwindcss'
 
 import daisyui from 'daisyui'
+import plugin from 'tailwindcss/plugin'
+
+import * as colors from './color'
+
+/**
+ * TODO: refactor with https://gmicloud.atlassian.net/browse/CE-583
+ */
+const rootStylePlugin = plugin(({ addBase }) => {
+  addBase({
+    ':root': {
+      '--font-inter': 'Inter, sans-serif',
+      '--transparent': 'transparent',
+    },
+  })
+})
+
+const scrollbarPlugin = plugin(({ addUtilities }) => {
+  const newUtilities = {
+    '.scrollbar-stable': {
+      'scrollbar-gutter': 'stable',
+    },
+  }
+  addUtilities(newUtilities)
+})
+
+const customClassesPlugin = plugin(({ addUtilities }) => {
+  addUtilities({
+    '.scrollbar-hide': {
+      /* Firefox */
+      'scrollbar-width': 'none',
+
+      /* Safari and Chrome */
+      '&::-webkit-scrollbar': {
+        display: 'none',
+      },
+    },
+
+    '.scrollbar-default': {
+      /* Firefox */
+      'scrollbar-width': 'auto',
+
+      /* Safari and Chrome */
+      '&::-webkit-scrollbar': {
+        display: 'block',
+      },
+    },
+    '.text-fill-transparent': {
+      'background-clip': 'text',
+      '-webkit-text-fill-color': 'transparent',
+    },
+    '.text-highlight-primary': {
+      'background-clip': 'text',
+      '-webkit-text-fill-color': 'transparent',
+      'background-image':
+        'linear-gradient(349.68deg, #a2ccf8 14.18%, #1780ed 47.37%, #5da6f3 76.79%)',
+    },
+  })
+})
 
 export default {
   theme: {
-    extend: {},
+    extend: {
+      fontSize: {
+        md: '22px',
+      },
+      colors: {
+        ...colors,
+      },
+      borderRadius: {
+        '0.5xl': '0.675rem',
+      },
+      zIndex: {
+        1: '1',
+        2: '2',
+        3: '3',
+        4: '4',
+        5: '5',
+        6: '6',
+        7: '7',
+        8: '8',
+        9: '9',
+        header: '101',
+        footer: '102',
+        modal: '103',
+      },
+    },
   },
   daisyui: {
     themes: ['winter'],
   },
-  plugins: [daisyui],
+  plugins: [daisyui, rootStylePlugin, customClassesPlugin, scrollbarPlugin],
 } satisfies Omit<Config, 'content'>
