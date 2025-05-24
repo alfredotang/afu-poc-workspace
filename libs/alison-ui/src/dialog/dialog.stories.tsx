e990cba54526762bc1fd7640c282e8ea2f1d3318
Alfredo Tang <asdzxcca0617@gmail.com>
2025-05-24 13:51:28 +0800

    feat(alison-ui): add dialog desc

--- libs/alison-ui/src/dialog/dialog.stories.tsx
+++ libs/alison-ui/src/dialog/dialog.stories.tsx
@@ -1,17 +1,75 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 import { Button } from '@alison-ui/button'
 
-import type { Meta } from '@storybook/react'
+import type { Meta, StoryObj } from '@storybook/react'
 
-import { SearchIcon } from 'lucide-react'
+import { SearchIcon, XIcon } from 'lucide-react'
 
 import * as Dialog from './components'
 
 export default {
   component: Dialog.Root,
   title: 'ui/dialog',
+  argTypes: {
+    title: {
+      control: 'text',
+    },
+    description: {
+      control: 'text',
+    },
+    closable: {
+      control: 'boolean',
+    },
+    closeIcon: {
+      control: 'object',
+    },
+    open: {
+      control: 'boolean',
+    },
+  },
 } satisfies Meta<typeof Dialog.Root>
+
+export const Default: StoryObj<typeof Dialog.Root> = {
+  args: {
+    title: 'Dialog Title',
+    description: 'This is a description for the dialog.',
+    closable: true,
+    closeIcon: <XIcon />,
+    open: false,
+  },
+  render: function Render({ open, ...args }) {
+    const [isOpen, setIsOpen] = useState(open)
+
+    useEffect(() => {
+      setIsOpen(open)
+    }, [open])
+
+    return (
+      <>
+        <Button onClick={() => setIsOpen(true)}>Click to open</Button>
+        <Dialog.Root open={isOpen} onOpenChange={setIsOpen} {...args}>
+          <Dialog.Content>
+            <div>
+              <p>
+                Dialog content goes here. You can place any React component
+                inside the dialog.
+              </p>
+            </div>
+            <Dialog.Footer>
+              <Dialog.Close>
+                <div className="flex gap-4">
+                  <Button variant="secondary">Close</Button>
+                  <Button>Confirm</Button>
+                </div>
+              </Dialog.Close>
+            </Dialog.Footer>
+          </Dialog.Content>
+        </Dialog.Root>
+      </>
+    )
+  },
+}
 
 export const Overview = () => (
   <Dialog.Root
